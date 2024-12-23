5fd9e1eb3832545282471923aaf050c271365908
Joe <joekuan@systex.com.tw>
2024-12-23 15:25:22 +0800

    safasfasfasf

--- src/js/showAnsPage.js
+++ src/js/showAnsPage.js
@@ -4,42 +4,42 @@ let config = [
         "src":
             [
                 {
-                    image: "../assets/photo/easy/1.jpg",
+                    image: "../assets/photo/easy/1.webp",
                     text1: "2021年1月5日",
                     text2: "【板橋】夏慕尼",
                 },
                 {
-                    image: "../assets/photo/easy/2.jpg",
+                    image: "../assets/photo/easy/2.webp",
                     text1: "2021年2月5日",
                     text2: "【大安】路地 氷の怪物",
                 },
                 {
-                    image: "../assets/photo/easy/3.jpg",
+                    image: "../assets/photo/easy/3.webp",
                     text1: "2021年12月23日",
                     text2: "【板橋】ABV 閣樓餐酒館",
                 },
                 {
-                    image: "../assets/photo/easy/4.jpg",
+                    image: "../assets/photo/easy/4.webp",
                     text1: "2022年1月6日",
                     text2: "【信義】焼肉の名門 赤虎",
                 },
                 {
-                    image: "../assets/photo/easy/5.jpg",
+                    image: "../assets/photo/easy/5.webp",
                     text1: "2022年8月12日",
                     text2: "【龜山】長庚大學",
                 },
                 {
-                    image: "../assets/photo/easy/6.jpg",
+                    image: "../assets/photo/easy/6.webp",
                     text1: "2023年1月5日",
                     text2: "【礁溪】東西匯歐陸料理",
                 },
                 {
-                    image: "../assets/photo/easy/7.jpg",
+                    image: "../assets/photo/easy/7.webp",
                     text1: "2023年5月17日",
                     text2: "【龜山】長庚大學",
                 },
                 {
-                    image: "../assets/photo/easy/8.jpg",
+                    image: "../assets/photo/easy/8.webp",
                     text1: "2024年5月18日",
                     text2: "【龜山】長庚大學",
                 },
@@ -50,42 +50,42 @@ let config = [
         "src":
             [
                 {
-                    image: "../assets/photo/medium/9.jpg",
+                    image: "../assets/photo/medium/9.webp",
                     text1: "2021年12月2日",
                     text2: "【中山】中山商圈",
                 },
                 {
-                    image: "../assets/photo/medium/10.jpg",
+                    image: "../assets/photo/medium/10.webp",
                     text1: "2021年12月9日",
                     text2: "【信義】信義A8商圈",
                 },
                 {
-                    image: "../assets/photo/medium/11.jpg",
+                    image: "../assets/photo/medium/11.webp",
                     text1: "2022年2月17日",
                     text2: "【士林】臺北燈節",
                 },
                 {
-                    image: "../assets/photo/medium/12.jpg",
+                    image: "../assets/photo/medium/12.webp",
                     text1: "2022年2月24日",
                     text2: "【中壢】桃園老街溪燈節",
                 },
                 {
-                    image: "../assets/photo/medium/13.jpg",
+                    image: "../assets/photo/medium/13.webp",
                     text1: "2022年11月22日",
                     text2: "【板橋】新北耶誕城",
                 },
                 {
-                    image: "../assets/photo/medium/14.jpg",
+                    image: "../assets/photo/medium/14.webp",
                     text1: "2023年6月15日",
                     text2: "【彰化】有片森林「植一座咖啡館」",
                 },
                 {
-                    image: "../assets/photo/medium/15.jpg",
+                    image: "../assets/photo/medium/15.webp",
                     text1: "2023年8月19日",
                     text2: "【松山】Roller 186",
                 },
                 {
-                    image: "../assets/photo/medium/16.jpg",
+                    image: "../assets/photo/medium/16.webp",
                     text1: "2023年10月14日",
                     text2: "【松山】75度獴",
                 },
@@ -96,42 +96,42 @@ let config = [
         "src":
             [
                 {
-                    image: "../assets/photo/hard/17o.jpg",
+                    image: "../assets/photo/hard/17o.webp",
                     text1: "2020年8月20日",
                     text2: "【青埔】Xpark",
                 },
                 {
-                    image: "../assets/photo/hard/18o.jpg",
+                    image: "../assets/photo/hard/18o.webp",
                     text1: "2021年1月1日",
                     text2: "【信義】統一時代百貨",
                 },
                 {
-                    image: "../assets/photo/hard/19o.jpg",
+                    image: "../assets/photo/hard/19o.webp",
                     text1: "2021年4月2日",
                     text2: "【文山】木柵動物園",
                 },
                 {
-                    image: "../assets/photo/hard/20o.jpg",
+                    image: "../assets/photo/hard/20o.webp",
                     text1: "2021年11月4日",
                     text2: "【大園】桃園青埔萬聖城",
                 },
                 {
-                    image: "../assets/photo/hard/21o.jpg",
+                    image: "../assets/photo/hard/21o.webp",
                     text1: "2022年7月26日",
                     text2: "【基隆】八斗子高地",
                 },
                 {
-                    image: "../assets/photo/hard/22o.jpg",
+                    image: "../assets/photo/hard/22o.webp",
                     text1: "2022年8月11日",
                     text2: "【萬華】Makers'Base",
                 },
                 {
-                    image: "../assets/photo/hard/23o.jpg",
+                    image: "../assets/photo/hard/23o.webp",
                     text1: "2023年6月14日",
                     text2: "【南投】星月天空夜景",
                 },
                 {
-                    image: "../assets/photo/hard/24o.jpg",
+                    image: "../assets/photo/hard/24o.webp",
                     text1: "2023年6月29日",
                     text2: "【烏來】瀑布3號",
                 },
